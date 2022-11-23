e168224d9d88b9427928e211fba001e0534b40b7
Htet Naing <htetnaing0814@gmail.com>
2022-11-23 14:56:48 +0630

    added vercel frontend to the cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -21,4 +21,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
+
+  allow do
+    origins "https://earn2learn.vercel.app"
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
 end
