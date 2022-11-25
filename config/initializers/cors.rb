69f5dbcbb773e354db5b4830feb541918d35aa08
Jerry <54890279+macabrepanpapakhin@users.noreply.github.com>
2022-11-25 11:07:07 +0630

    update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,7 +15,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins "http://etl.robust.best/"
+    origins "https://etl.robust.best/"
 
     resource "*",
       headers: :any,
