ec18982f9be3fdf9957f25aeb8bf58b5376347a1
Htet Naing <htetnaing0814@gmail.com>
2022-11-26 14:02:15 +0630

    Login with Either Username or Email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
   # POST /users/login
   def login
-    user = User.find_by(email: params[:email])
+    user = User.where('username LIKE :query OR email LIKE :query', query: "%#{params[:auth]}%").first
     if user
       if user.authenticate(params[:password])
         token = encode_token({ user_id: user.id })
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,6 +7,14 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
+    origins "*"
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
+  allow do
     origins "http://127.0.0.1:5173/"
 
     resource "*",
